05cf5fea9fc228d0ac52a1dd12aa15d1de7602e5
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-30 00:19:48 +0100

    add transaction build unit test

--- test/wallet_test/transaction_test.dart
+++ test/wallet_test/transaction_test.dart
@@ -12,43 +12,35 @@ import 'package:hex/hex.dart';
 
 void main() {
   group("test hd", () {
-    setupServiceLocator();
-    FlutterConfig.loadValueForTesting(
-        {'API_URL': 'https://dev-supernode.defichain-wallet.com'});
     test("get transaction", () async {
-      var mnemonic =
-          'sample visa rain lab truly dwarf hospital uphold stereo ride combine arrest aspect exist oil just boy garment estate enable marriage coyote blue yellow';
-      final apiService = ApiService();
-      var seed = mnemonicToSeedHex(mnemonic);
-
-      var wallet = new WalletAccount(account: 0, chain: ChainType.DeFiChain);
-      var transaction = await WalletSync.syncTransactions(ChainType.DeFiChain,
-          ChainNet.Testnet, mnemonic, "", apiService, [wallet]);
-      var unspentTxs = transaction.where((element) => element.spentHeight < 0);
-
-      var unspentTx = unspentTxs.first;
-
-      var networkType =
+      var network =
           HdWalletUtil.getNetworkType(ChainType.DeFiChain, ChainNet.Testnet);
 
-      final alice = HdWalletUtil.getKeyPair(HEX.decode(seed), unspentTx.account,
-          false, unspentTx.index, ChainType.DeFiChain, ChainNet.Testnet);
+      final alice = ECPair.fromWIF(
+          "cNpueJjp8geQJut28fDyUD8e5zoyctHxj9GE8rTbQXwiEwLo1kq4",
+          network: network);
+
       final p2wpkh = P2WPKH(data: PaymentData(pubkey: alice.publicKey)).data;
       final redeemScript = p2wpkh.output;
 
-      final txb = TransactionBuilder(network: networkType);
-      txb.setVersion(1);
-      txb.addInput(unspentTx.mintTxId, 0);
-      txb.addOutput('tbTMwPQAtLUYCxHjPRc9upUmHBdGFr8cKN', 10);
+      final txb = TransactionBuilder(network: network);
+      txb.setVersion(2);
+      txb.setLockTime(0);
+
+      txb.addInput(
+          'eca1a534a6e9349a77d914b9e593492a9cac58ff9e974aaf5e2353909df367af',
+          0);
+      txb.addOutput('tbTMwPQAtLUYCxHjPRc9upUmHBdGFr8cKN', 27999796096 - 200);
 
       txb.sign(
           vin: 0,
           keyPair: alice,
-          redeemScript: redeemScript,
-          witnessValue: 80000);
-      final txhex = txb.build().toHex();
-
-      debugPrint("test");
+          witnessValue: 27999796096,
+          redeemScript: redeemScript);
+      final tx = txb.build();
+      final txhex = tx.toHex();
+      expect(txhex,
+          "02000000000101af67f39d9053235eaf4a979eff58ac9c2a4993e5b914d9779a34e9a634a5a1ec000000001716001421cf7b9e2e17fa2879be2a442d8454219236bd3affffffff01b8faea840600000017a91438f4a35256ea1afcf989eb28bcdfcc082157ac7987024830450221009102f2f177590e96c4e6f9e5269f3fadff98a6b9a2d2fc157666a12926c4b8fa022014620215f6ec933593069962d6f27c5215b013be3bfcc0aea12c8b9011f77e0e012103352705381be729d234e692a6ee4bf9e2800b9fc1ef0ebc96b6cf35c38658c93c00000000");
     });
   });
 }

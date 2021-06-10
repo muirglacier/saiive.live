e590d5bdc22a46e3e991eefaaefd52854c659213
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-06-10 21:57:48 +0200

    update signing

--- .devops/android_key_properties.sh
+++ .devops/android_key_properties.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -f android/key.properties.template
-echo storePassword=$1 >> android/key.properties.template
-echo keyPassword=$1 >> android/key.properties.template
-echo keyAlias=upload >> android/key.properties.template
-echo storeFile=$2 >> android/key.properties.template
+rm -f android/key.properties
+echo storePassword=$1 >> android/key.properties
+echo keyPassword=$1 >> android/key.properties
+echo keyAlias=upload >> android/key.properties
+echo storeFile=$2 >> android/key.properties
 
 cat android/key.properties

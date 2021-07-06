f8a0fe634ca78f78157c4c75125825b5c85fcef0
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-07-06 17:03:41 +0200

    add base screen

--- lib/ui/expert/expert_screen.dart
+++ lib/ui/expert/expert_screen.dart
@@ -7,8 +7,12 @@ class ExpertScreen extends StatefulWidget {
 }
 
 class _ExpertScreen extends State<ExpertScreen> {
+  _buildExpertScreen(BuildContext context) {
+    return Center(child: Text("TODO!"));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Expert mode")));
+    return Scaffold(appBar: AppBar(title: Text("Expert mode")), body: _buildExpertScreen(context));
   }
 }

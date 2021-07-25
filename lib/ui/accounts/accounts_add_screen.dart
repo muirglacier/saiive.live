4bd74e3a1e160f84e9fcceefc70480d8d9f0fc0e
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-07-25 09:15:00 +0200

    allow edit of account name

--- lib/ui/accounts/accounts_add_screen.dart
+++ lib/ui/accounts/accounts_add_screen.dart
@@ -1,26 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:saiive.live/appcenter/appcenter.dart';
 import 'package:saiive.live/appstate_container.dart';
 import 'package:saiive.live/crypto/chain.dart';
+import 'package:saiive.live/crypto/model/wallet_account.dart';
 import 'package:saiive.live/generated/l10n.dart';
+import 'package:saiive.live/service_locator.dart';
+import 'package:saiive.live/services/wallet_service.dart';
 
 class AccountsAddScreen extends StatefulWidget {
-  final ChainType chainType;
+  final WalletAccount walletAccount;
+  final bool isNewAddress;
 
-  AccountsAddScreen(this.chainType);
+  AccountsAddScreen(this.walletAccount, this.isNewAddress);
 
   @override
   State<StatefulWidget> createState() => _AccountsAddScreen();
 }
 
 class _AccountsAddScreen extends State<AccountsAddScreen> {
+  final _nameController = TextEditingController();
+
+  _init() {
+    if (!widget.isNewAddress) {
+      _nameController.text = widget.walletAccount.name;
+      setState(() {});
+    }
+  }
+
   @override
   initState() {
     super.initState();
+    _init();
   }
 
   _buildAccountAddScreen(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(hintText: S.of(context).label),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                try {
+                  if (_nameController.text != null && _nameController.text.isNotEmpty) {
+                    final walletService = sl.get<IWalletService>();
+
+                    widget.walletAccount.name = _nameController.text;
+                    await walletService.addAccount(widget.walletAccount);
+
+                    Navigator.of(context).pop();
+                  }
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(S.of(context).wallet_offline(e.toString()))));
+                  sl.get<AppCenterWrapper>().trackEvent("addAccountError", <String, String>{'error': e.toString()});
+                }
+              },
+              child: Text(widget.isNewAddress ? S.of(context).add : S.of(context).save),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -28,9 +75,9 @@ class _AccountsAddScreen extends State<AccountsAddScreen> {
     return Scaffold(
         appBar: AppBar(
           toolbarHeight: StateContainer.of(context).curTheme.toolbarHeight,
-          title: Text(S.of(context).wallet_accounts_address_add),
+          title: Text(widget.isNewAddress ? S.of(context).wallet_accounts_add : S.of(context).wallet_accounts_edit),
           actions: [],
         ),
-        body: _buildAccountAddScreen(context));
+        body: SingleChildScrollView(child: _buildAccountAddScreen(context)));
   }
 }

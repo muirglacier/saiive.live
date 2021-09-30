304c0d68898fb28c7c9fe1d26e53597c729b6628
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-09-30 16:44:36 +0200

    improve ui/ux

--- lib/ui/accounts/accounts_wallet_address_export_private_key.dart
+++ lib/ui/accounts/accounts_wallet_address_export_private_key.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:clipboard_manager/clipboard_manager.dart';
 import 'package:flutter/services.dart';
@@ -56,9 +57,10 @@ class _AccountsWalletAddressExportPrivateKeyWidgetState extends State<AccountsWa
           });
           _timer.cancel();
           _timer = null;
-
-          await ClipboardManager.copyToClipBoard("https://www.saiive.live");
-          await Clipboard.setData(new ClipboardData(text: "https://www.saiive.live"));
+          if (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia) {
+            await ClipboardManager.copyToClipBoard("https://www.saiive.live");
+            await Clipboard.setData(new ClipboardData(text: "https://www.saiive.live"));
+          }
         }
       },
     );
@@ -71,66 +73,76 @@ class _AccountsWalletAddressExportPrivateKeyWidgetState extends State<AccountsWa
     _init();
   }
 
+  Widget buildPrivateKeyPart(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.start, children: [
+      ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
+          child: QrImage(
+            data: _privateKeyWif,
+            version: QrVersions.auto,
+            errorCorrectionLevel: QrErrorCorrectLevel.H,
+            foregroundColor: StateContainer.of(context).curTheme.text,
+          )),
+      SelectableText(_privateKeyWif)
+    ]);
+  }
+
+  Widget buildBlurPage(BuildContext context) {
+    var height = MediaQuery.of(context).size.height * 0.4;
+
+    return Column(children: [
+      if (!_showPrivateKey)
+        Blur(
+            blur: _showPrivateKey ? 0 : 20,
+            colorOpacity: _showPrivateKey ? 0.0 : 0.5,
+            blurColor: Theme.of(context).primaryColor,
+            child: SizedBox(
+                height: height,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 20, top: 10),
+                  child: buildPrivateKeyPart(context),
+                ))),
+      if (_showPrivateKey) buildPrivateKeyPart(context),
+      if (!_showPrivateKey)
+        ElevatedButton(
+            onPressed: () async {
+              sl.get<AuthenticationHelper>().forceAuth(context, () {
+                _startTimer();
+                setState(() {
+                  _showPrivateKey = true;
+                });
+              });
+            },
+            child: Text(S.of(context).show)),
+      if (_showPrivateKey)
+        if (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia)
+          ElevatedButton(
+              onPressed: () async {
+                sl.get<AuthenticationHelper>().forceAuth(context, () async {
+                  await ClipboardManager.copyToClipBoard(_privateKeyWif);
+                  ScaffoldMessenger.of(NavigationHelper.navigatorKey.currentContext).showSnackBar(SnackBar(
+                    content: Text(S.of(context).receive_address_copied_to_clipboard),
+                  ));
+
+                  await Clipboard.setData(new ClipboardData(text: _privateKeyWif));
+                });
+              },
+              child: Text(S.of(context).copy)),
+      if (_showPrivateKey) Text(_displayTimerTicker.toString())
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_privateKeyWif.isEmpty) {
       return LoadingWidget(text: S.of(context).loading);
     }
 
-    var height = MediaQuery.of(context).size.height * 0.4;
     return Scaffold(
         appBar: AppBar(
           toolbarHeight: StateContainer.of(context).curTheme.toolbarHeight,
           title: Text(S.of(context).wallet_account_export_private_key),
         ),
-        body: Padding(
-            padding: EdgeInsets.all(30),
-            child: Column(children: [
-              Blur(
-                  blur: _showPrivateKey ? 0 : 20,
-                  colorOpacity: _showPrivateKey ? 0.0 : 0.5,
-                  blurColor: Theme.of(context).primaryColor,
-                  child: SizedBox(
-                      height: height,
-                      child: Padding(
-                        padding: EdgeInsets.only(bottom: 20, top: 10),
-                        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.start, children: [
-                          ConstrainedBox(
-                              constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
-                              child: QrImage(
-                                data: _privateKeyWif,
-                                version: QrVersions.auto,
-                                errorCorrectionLevel: QrErrorCorrectLevel.H,
-                                foregroundColor: StateContainer.of(context).curTheme.text,
-                              )),
-                          SelectableText(_privateKeyWif)
-                        ]),
-                      ))),
-              if (!_showPrivateKey)
-                ElevatedButton(
-                    onPressed: () async {
-                      sl.get<AuthenticationHelper>().forceAuth(context, () {
-                        _startTimer();
-                        setState(() {
-                          _showPrivateKey = true;
-                        });
-                      });
-                    },
-                    child: Text(S.of(context).show)),
-              if (_showPrivateKey)
-                ElevatedButton(
-                    onPressed: () async {
-                      sl.get<AuthenticationHelper>().forceAuth(context, () async {
-                        await ClipboardManager.copyToClipBoard(_privateKeyWif);
-                        ScaffoldMessenger.of(NavigationHelper.navigatorKey.currentContext).showSnackBar(SnackBar(
-                          content: Text(S.of(context).receive_address_copied_to_clipboard),
-                        ));
-
-                        await Clipboard.setData(new ClipboardData(text: _privateKeyWif));
-                      });
-                    },
-                    child: Text(S.of(context).copy)),
-              if (_showPrivateKey) Text(_displayTimerTicker.toString())
-            ])));
+        body: Padding(padding: EdgeInsets.all(30), child: Center(child: buildBlurPage(context))));
   }
 }

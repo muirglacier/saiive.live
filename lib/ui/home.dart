4124de6ac1181d0a66c241f1a4da5950703f99a1
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-11-16 09:45:29 +0100

    make navbar scrollable

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -27,12 +27,7 @@ class NavigationEntry {
   final bool visibleForBottomNav;
   final String routeSettingName;
 
-  NavigationEntry(
-      {this.icon,
-      this.label,
-      this.page,
-      this.visibleForBottomNav = true,
-      this.routeSettingName});
+  NavigationEntry({this.icon, this.label, this.page, this.visibleForBottomNav = true, this.routeSettingName});
 }
 
 class HomeScreen extends StatefulWidget {
@@ -51,20 +46,9 @@ class _HomeScreenState extends State<HomeScreen> {
     if (_navigationEntries.isEmpty) {
       _navigationEntries = [
         NavigationEntry(
-            icon: Icon(Icons.account_balance_wallet),
-            label: S.of(context).home_wallet,
-            page: WalletHomeScreen(key: PageStorageKey('WalletHome')),
-            routeSettingName: "/home"),
-        NavigationEntry(
-            icon: Icon(Icons.pie_chart),
-            label: S.of(context).home_liquidity,
-            page: LiquidityScreen(key: PageStorageKey('Liquidity')),
-            routeSettingName: "/liqudity"),
-        NavigationEntry(
-            icon: Icon(Icons.compare_arrows),
-            label: S.of(context).home_dex,
-            page: DexScreen(key: PageStorageKey('DEX')),
-            routeSettingName: "/dex"),
+            icon: Icon(Icons.account_balance_wallet), label: S.of(context).home_wallet, page: WalletHomeScreen(key: PageStorageKey('WalletHome')), routeSettingName: "/home"),
+        NavigationEntry(icon: Icon(Icons.pie_chart), label: S.of(context).home_liquidity, page: LiquidityScreen(key: PageStorageKey('Liquidity')), routeSettingName: "/liqudity"),
+        NavigationEntry(icon: Icon(Icons.compare_arrows), label: S.of(context).home_dex, page: DexScreen(key: PageStorageKey('DEX')), routeSettingName: "/dex"),
         NavigationEntry(
             icon: Icon(Icons.account_box),
             label: S.of(context).wallet_accounts,
@@ -116,56 +100,50 @@ class _HomeScreenState extends State<HomeScreen> {
       )));
     }
 
-    final List<NavigationRailDestination> navBar = _navigationEntries
-        .map((e) =>
-            NavigationRailDestination(icon: e.icon, label: Text(e.label)))
-        .toList();
+    final List<NavigationRailDestination> navBar = _navigationEntries.map((e) => NavigationRailDestination(icon: e.icon, label: Text(e.label))).toList();
     var currentEnvironment = EnvHelper.getEnvironment();
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        NavigationRail(
-            backgroundColor: StateContainer.of(context).curTheme.lightColor,
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            labelType: NavigationRailLabelType.all,
-            selectedIconTheme: IconThemeData(
-                color: StateContainer.of(context).curTheme.primary),
-            selectedLabelTextStyle:
-                TextStyle(color: StateContainer.of(context).curTheme.primary),
-            leading: Container(
-                child: Column(children: [
-              SizedBox(
-                  width: 200,
-                  child: Column(children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(S.of(context).wallet_home_network,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            SizedBox(height: 5),
-                            VersionWidget(),
-                            if (currentEnvironment !=
-                                EnvironmentType.Production)
-                              Text(EnvHelper.environmentToString(
-                                  currentEnvironment))
-                          ]),
-                    ),
-                    Divider(
-                        color:
-                            StateContainer.of(context).curTheme.backgroundColor)
-                  ]))
-            ])),
-            destinations: navBar),
+        LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+              child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                      child: NavigationRail(
+                          backgroundColor: StateContainer.of(context).curTheme.lightColor,
+                          selectedIndex: _selectedIndex,
+                          onDestinationSelected: (int index) {
+                            setState(() {
+                              _selectedIndex = index;
+                            });
+                          },
+                          labelType: NavigationRailLabelType.all,
+                          selectedIconTheme: IconThemeData(color: StateContainer.of(context).curTheme.primary),
+                          selectedLabelTextStyle: TextStyle(color: StateContainer.of(context).curTheme.primary),
+                          leading: Container(
+                              child: Column(children: [
+                            SizedBox(
+                                width: 200,
+                                child: Column(children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+                                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.start, children: [
+                                      Text(S.of(context).wallet_home_network, style: TextStyle(fontWeight: FontWeight.bold)),
+                                      SizedBox(height: 5),
+                                      VersionWidget(),
+                                      if (currentEnvironment != EnvironmentType.Production) Text(EnvHelper.environmentToString(currentEnvironment))
+                                    ]),
+                                  ),
+                                  Divider(color: StateContainer.of(context).curTheme.backgroundColor)
+                                ]))
+                          ])),
+                          destinations: navBar))));
+        }),
         VerticalDivider(thickness: 1, width: 1),
         // This is the main content.
         Expanded(
@@ -182,10 +160,8 @@ class _HomeScreenState extends State<HomeScreen> {
       return null;
     }
 
-    final List<BottomNavigationBarItem> bottomNavBar = _navigationEntries
-        .where((a) => a.visibleForBottomNav)
-        .map((e) => BottomNavigationBarItem(icon: e.icon, label: e.label))
-        .toList();
+    final List<BottomNavigationBarItem> bottomNavBar =
+        _navigationEntries.where((a) => a.visibleForBottomNav).map((e) => BottomNavigationBarItem(icon: e.icon, label: e.label)).toList();
 
     return BottomNavigationBar(
       items: bottomNavBar,
@@ -205,9 +181,7 @@ class _HomeScreenState extends State<HomeScreen> {
       });
       Navigator.pop(context);
     } else {
-      Navigator.of(context).push(MaterialPageRoute(
-          settings: RouteSettings(name: nav.routeSettingName),
-          builder: (BuildContext context) => nav.page));
+      Navigator.of(context).push(MaterialPageRoute(settings: RouteSettings(name: nav.routeSettingName), builder: (BuildContext context) => nav.page));
     }
   }
 

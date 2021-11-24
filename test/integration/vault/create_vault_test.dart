e1d204d9a15afd3de2b9a5a344c11b8fb17fbcf8
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-11-24 15:00:43 +0100

    disable integration tests for now

--- test/integration/vault/create_vault_test.dart
+++ test/integration/vault/create_vault_test.dart
@@ -8,24 +8,24 @@ void main() async {
       "sample visa rain lab truly dwarf hospital uphold stereo ride combine arrest aspect exist oil just boy garment estate enable marriage coyote blue yellow");
 
   group("#1 integration vault", () {
-    String vaultId;
-    test("#0 init", () async {
-      await baseInit();
-    });
-    test("#1 create vault", () async {
-      final wallet = sl.get<DeFiChainWallet>();
+    // String vaultId;
+    // test("#0 init", () async {
+    //   await baseInit();
+    // });
+    // test("#1 create vault", () async {
+    //   final wallet = sl.get<DeFiChainWallet>();
 
-      vaultId = await wallet.createVault("C1000", 100000000, ownerAddress: "tXmZ6X4xvZdUdXVhUKJbzkcN2MNuwVSEWv");
-    }, timeout: Timeout(Duration(minutes: 10)));
+    //   vaultId = await wallet.createVault("C1000", 100000000, ownerAddress: "tXmZ6X4xvZdUdXVhUKJbzkcN2MNuwVSEWv");
+    // }, timeout: Timeout(Duration(minutes: 10)));
 
-    test("#1 deposit to vault", () async {
-      final wallet = sl.get<DeFiChainWallet>();
+    // test("#1 deposit to vault", () async {
+    //   final wallet = sl.get<DeFiChainWallet>();
 
-      await wallet.depositToVault(vaultId, "tXmZ6X4xvZdUdXVhUKJbzkcN2MNuwVSEWv", "DFI", 10 * 100000000);
-    }, timeout: Timeout(Duration(minutes: 10)));
+    //   await wallet.depositToVault(vaultId, "tXmZ6X4xvZdUdXVhUKJbzkcN2MNuwVSEWv", "DFI", 10 * 100000000);
+    // }, timeout: Timeout(Duration(minutes: 10)));
 
-    test("#99 destroy", () async {
-      await destoryTest();
-    });
+    // test("#99 destroy", () async {
+    //   await destoryTest();
+    // });
   });
 }

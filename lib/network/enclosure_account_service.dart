c039bdd198303d18321605a8ad9bd5af78487fcf
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-12-15 21:55:49 +0100

    remove isolated parsing

--- lib/network/enclosure_account_service.dart
+++ lib/network/enclosure_account_service.dart
@@ -6,7 +6,6 @@ import 'package:saiive.live/network/model/key_account_wrapper.dart';
 import 'package:saiive.live/network/request/addresses_request.dart';
 import 'package:saiive.live/network/response/error_response.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/foundation.dart';
 
 class EnclosureAccountService {
   final serverAddress;
@@ -14,10 +13,6 @@ class EnclosureAccountService {
   String baseUri = "/api/v1/";
 
   EnclosureAccountService(this.serverAddress, this.network);
-
-  static Future<dynamic> _parseJson(String str) async {
-    return json.decode(str);
-  }
 
   Future<dynamic> makeHttpPostRequest(String url, String coin, dynamic request) async {
     final finalUrl = this.serverAddress + baseUri + network + "/" + coin + url;
@@ -36,9 +31,7 @@ class EnclosureAccountService {
     var response = await makeHttpPostRequest('/accounts', coin, request);
 
     if (response is Response) {
-      final decoded = await compute(EnclosureAccountService._parseJson, response.body);
-
-      List<KeyAccountWrapper> keyAccountWrappers = decoded.map<KeyAccountWrapper>((data) => KeyAccountWrapper.fromJson(data)).toList();
+      List<KeyAccountWrapper> keyAccountWrappers = json.decode(response.body).map<KeyAccountWrapper>((data) => KeyAccountWrapper.fromJson(data)).toList();
 
       return keyAccountWrappers;
     }
